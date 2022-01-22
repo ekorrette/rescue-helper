9665e03a8e35a05a738cfbe0461399b31a8bb20c
nicolechoong <37162961+nicolechoong@users.noreply.github.com>
2022-01-22 21:30:02 +0000

    Create sms.py

---
+++ src/sms.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, session, url_for, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_survey():
+    response = MessagingResponse()
+    from_number = request.values.get('From')
+
+    if (from_number not in session.keys()):
+        response.message("Please enter your address")
+        session[from_number] = {'address': None, 'capacity': None}
+
+    elif (session[from_number]['address'] is None):
+        session[from_number]['address'] = request.values.get('Body')
+        response.message("How many humanz please")
+    elif (session[from_number]['capacity'] is None):
+        session[from_number]['capacity'] = request.values.get('Body')
+        response.message("Thank you for your response.")
+    else:
+        response.message("Address: {}\nCapacity: {}").format(session[from_number]['address'], session[from_number]['capacity'])
+
+    return str(response)
