5f4d2c26dcb465cf9246bc54ab13cfb08e15d86f
KubinGH <KubinGH@users.noreply.github.com>
2022-01-22 23:08:10 +0000

    Load data from server and simple auto-reload

--- src/client.py
+++ src/client.py
@@ -1,29 +1,55 @@
 import geopy
 import plotly.express as px
 import pandas as pd
+import time
+import requests
+import json
 import planner
 
 geolocator = geopy.geocoders.Nominatim(user_agent='sendhelp')
+px.set_mapbox_access_token(open("../secret/mapbox_token").read())
 
-addrs = [
+default_addrs = [
     '4 Trinity Lane Cambridge', 'North Paddock Cambridge', 'Cambridge South Paddock',
     'Cambridge Cambridge Bridge Street 12 UK', 'Cambridge 12 Round Church Street',
     '1 Grange Road CB2 9AS'
 ]
 
-gcs = [geolocator.geocode(addr) for addr in addrs]
 
-df = pd.DataFrame({'lat': gc.latitude, 'lon': gc.longitude, 'address': gc.address} for gc in gcs)
-
-cl = planner.cluster(df[["lat", "lon"]], 3)
-k = len(cl.cluster_centers_)
+def get_data():
+    return json.loads(requests.get('http://9272-188-39-25-218.ngrok.io/data').content)
 
 
-px.set_mapbox_access_token(open("../secret/mapbox_token").read())
+def address_plot_data(datapoints):
+    ok = []
+    for key, value in datapoints.items():
+        try:
+            gc = geolocator.geocode(value['address'])
+        except ValueError:
+            pass
+        else:
+            if gc is not None:
+                value = value.copy()
+                value['gc'] = gc
+                value['name'] = key
+                print(value)
+                ok.append(value)
+    data = pd.DataFrame({
+        'lat': d['gc'].latitude, 'lon': d['gc'].longitude,
+        'address': d['gc'].address, 'name': d['name'],
+        'capacity': d['capacity']
+    } for d in ok)
+    clustering = planner.cluster(data[["lat", "lon"]], 3)
+    return clustering, data
+
 
 colors = px.colors.qualitative.Dark24
-fig = px.scatter_mapbox(df, lat="lat", lon="lon", hover_name="address",
-                        color=[colors[l % len(colors)] for l in cl.labels_], zoom=16, size=[15]*len(df))
-fig.update_layout(mapbox_style="basic")
-fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-fig.show()
+
+while True:
+    cl, df = address_plot_data(get_data())
+    fig = px.scatter_mapbox(df, lat='lat', lon='lon', hover_name='name', custom_data=['address'],
+                            color=[colors[l % len(colors)] for l in cl.labels_], zoom=16, size=6*df['capacity'])
+    fig.update_layout(mapbox_style="basic")
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig.show()
+    time.sleep(5)
--- src/planner.py
+++ src/planner.py
@@ -3,5 +3,5 @@ import sklearn.metrics
 
 
 def cluster(points, k=None):
-    return sklearn.cluster.KMeans(n_clusters=k).fit(points)
+    return sklearn.cluster.KMeans(n_clusters=min(k, len(points))).fit(points)
 
