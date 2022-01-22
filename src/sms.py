19db8a771776d4832acf91a46dc88e3cd9c3ad33
nicolechoong <37162961+nicolechoong@users.noreply.github.com>
2022-01-22 21:51:21 +0000

    Update sms.py

--- src/sms.py
+++ src/sms.py
@@ -9,16 +9,19 @@ def sms_survey():
     from_number = request.values.get('From')
 
     if (from_number not in session.keys()):
-        response.message("Please enter your address")
+        response.message("Please enter your address.")
         session[from_number] = {'address': None, 'capacity': None}
 
     elif (session[from_number]['address'] is None):
         session[from_number]['address'] = request.values.get('Body')
-        response.message("How many humanz please")
+        response.message("How many humanz please.")
+        print(session)
+        
     elif (session[from_number]['capacity'] is None):
         session[from_number]['capacity'] = request.values.get('Body')
         response.message("Thank you for your response.")
+        
     else:
-        response.message("Address: {}\nCapacity: {}").format(session[from_number]['address'], session[from_number]['capacity'])
+        response.message(("Address: {}\nCapacity: {}").format(session[from_number]['address'], session[from_number]['capacity']))
 
     return str(response)
