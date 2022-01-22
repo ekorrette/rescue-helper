d3833d01567cca5a6bdd13cad0f95bd85bd34271
KubinGH <KubinGH@users.noreply.github.com>
2022-01-22 22:44:39 +0000

    😔

--- src/client.py
+++ src/client.py
@@ -7,7 +7,8 @@ geolocator = geopy.geocoders.Nominatim(user_agent='sendhelp')
 
 addrs = [
     '4 Trinity Lane Cambridge', 'North Paddock Cambridge', 'Cambridge South Paddock',
-    'Cambridge Cambridge Bridge Street 12 UK', 'Cambridge 12 Round Church Street'
+    'Cambridge Cambridge Bridge Street 12 UK', 'Cambridge 12 Round Church Street',
+    '1 Grange Road CB2 9AS'
 ]
 
 gcs = [geolocator.geocode(addr) for addr in addrs]
@@ -22,7 +23,7 @@ px.set_mapbox_access_token(open("../secret/mapbox_token").read())
 
 colors = px.colors.qualitative.Dark24
 fig = px.scatter_mapbox(df, lat="lat", lon="lon", hover_name="address",
-                        color=[colors[l % len(colors)] for l in cl.labels_], zoom=10, size=[15]*len(df))
+                        color=[colors[l % len(colors)] for l in cl.labels_], zoom=16, size=[15]*len(df))
 fig.update_layout(mapbox_style="basic")
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 fig.show()
