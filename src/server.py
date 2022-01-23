4726cc0e19901b7d95fc941dc5553145a762d4d9
Ekorrette <missmysss@gmail.com>
2022-01-23 03:36:30 +0000

    deepgram hello

--- src/server.py
+++ src/server.py
@@ -1,11 +1,19 @@
-from flask import Flask, request, url_for, redirect
+from flask import Flask, request, url_for, redirect, render_template
 from twilio.twiml.voice_response import VoiceResponse
 from twilio.twiml.messaging_response import MessagingResponse
+from deepgram import Deepgram
+import json, requests, asyncio
+
+import http.client
 
 #from dotenv import load_dotenv
 import os
 
+DEEPGRAM_API_KEY = open('../secret/deepgram_key').read().strip()
+
 app = Flask(__name__)
+
+URL = "http://9272-188-39-25-218.ngrok.io/"
 
 app.secret_key = os.urandom(12)
 log = {}
@@ -16,6 +24,7 @@ def database():
     """Return address database"""
     return data
 
+
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
     """Respond to incoming phone calls with a 'Hello world' message"""
@@ -23,9 +32,30 @@ def voice():
     resp = VoiceResponse()
 
     # Read a message aloud to the caller
-    resp.say("hello world!", voice='alice')
+    resp.say("hello world! say the stuff and press a key.", voice='alice')
+    resp.record(action=URL+'thanks',
+            recordingStatusCallback=URL+"completed")
 
     return str(resp)
+
+@app.route("/thanks", methods=['GET', 'POST'])
+def thanks():
+    resp = VoiceResponse()
+    resp.say("thanks", voice='alice')
+    return str(resp)
+
+
+@app.route("/completed", methods=['GET', 'POST'])
+def record_completed():
+    print(request.values)
+    url = request.values.get('RecordingUrl') + '.wav'
+    print(url)
+    process(url)
+
+    resp = VoiceResponse()
+    resp.say("your response has been processed", voice='alice')
+    return str(resp)
+
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_survey():
@@ -60,6 +90,42 @@ def sms_survey():
             from_number, data[from_number]['address'], data[from_number]['capacity']))
 
     return str(response)
+
+
+@app.route("/web", methods=['POST', 'GET'])
+def web():
+    message = ''
+    if request.method == 'POST':
+        number = request.form.get('number')  # access the data inside 
+        address = request.form.get('address')
+        capacity = request.form.get('capacity')
+        data[number] = {'address': address, 'capacity': capacity}
+        message = 'submitted'
+
+    return render_template('webform.html', message=message)
+
+@app.route("/test", methods=['POST', 'GET'])
+def test():
+    url = 'https://api.twilio.com/2010-04-01/Accounts/ACcfef691231a0b455b289bdd859eefd71/Recordings/RE50532561317f075f7e0c33c69d68ca4b' + '.wav'
+    process(url)
+    return ''
+
+def process(url):
+    conn = http.client.HTTPSConnection("api.deepgram.com")
+    payload = '{\"url\":\"' + url + '\"}'
+    headers = {
+        'content-type': "application/json",
+        'Authorization': "Token " + DEEPGRAM_API_KEY
+    }
+    print(payload)
+    conn.request("POST", "/v1/listen", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+    transcript = json.loads(data.decode("utf-8"))['results']['channels'][0]['alternatives'][0]['transcript']
+    print(transcript)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
