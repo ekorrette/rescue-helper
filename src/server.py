8a99c35c89e05eb11d0e468aef9bf343bc3161e0
nicolechoong <37162961+nicolechoong@users.noreply.github.com>
2022-01-23 10:52:30 +0000

    Update server.py

--- src/server.py
+++ src/server.py
@@ -96,25 +96,26 @@ def sms_survey():
     response = MessagingResponse()
     from_number = request.values.get('From')
 
-    if from_number not in log.keys():
-        response.message("Please enter your address.")
-        log[from_number] = {'address': None, 'capacity': None}
-
-    elif log[from_number]['address'] is None:
-        log[from_number]['address'] = request.values.get('Body')
-        response.message("How many people are stranded there?")
-
-    elif log[from_number]['capacity'] is None:
-        cap = request.values.get('Body')
-        try:
-            cap = int(cap)
-            if cap <= 0:
+    if from_number not in data.keys():
+        if from_number not in log.keys():
+            response.message("Please enter your address.")
+            log[from_number] = {'address': None, 'capacity': None}
+    
+        elif log[from_number]['address'] is None:
+            log[from_number]['address'] = request.values.get('Body')
+            response.message("How many people are stranded there?")
+    
+        elif log[from_number]['capacity'] is None:
+            cap = request.values.get('Body')
+            try:
+                cap = int(cap)
+                if cap <= 0:
+                    response.message("Please enter a valid number.\n\nHow many people are stranded there?")
+                else:
+                    log[from_number]['capacity'] = cap
+                    data[from_number] = log.pop(from_number)
+            except:
                 response.message("Please enter a valid number.\n\nHow many people are stranded there?")
-            else:
-                log[from_number]['capacity'] = cap
-                data[from_number] = log.pop(from_number)
-        except:
-            response.message("Please enter a valid number.\n\nHow many people are stranded there?")
 
     else:
         response.message(("Thank you for your response.\n\n"
