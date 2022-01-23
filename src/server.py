de8f2099c218705bdf7bceeca9a39ad708ae29a0
Ekorrette <missmysss@gmail.com>
2022-01-23 07:23:30 +0000

    pep

--- src/server.py
+++ src/server.py
@@ -18,18 +18,18 @@ URL = "https://9272-188-39-25-218.ngrok.io/"
 app.secret_key = os.urandom(12)
 log = {}
 data = {
-  "1": {
-    "address": "1 Grange Road, CB3 9AS, Cambridge", 
-    "capacity": "6"
-  }, 
-  "2": {
-    "address": "3 Grange Road, CB3 9AS, Cambridge", 
-    "capacity": "3"
-  }, 
-  "3": {
-    "address": "Selwyn College, CB3 9DQ", 
-    "capacity": "23"
-  }
+    "1": {
+        "address": "1 Grange Road, CB3 9AS, Cambridge",
+        "capacity": "6"
+    },
+    "2": {
+        "address": "3 Grange Road, CB3 9AS, Cambridge",
+        "capacity": "3"
+    },
+    "3": {
+        "address": "Selwyn College, CB3 9DQ",
+        "capacity": "23"
+    }
 }
 
 
@@ -49,8 +49,8 @@ def voice():
 
     # Read a message aloud to the caller
     resp.say("Please enter your home number, street and city. Then press any key.", voice='alice')
-    resp.record(action=URL+'address/'+number,
-            recordingStatusCallback=URL+"completed/address/"+number)
+    resp.record(action=URL + 'address/' + number,
+                recordingStatusCallback=URL + "completed/address/" + number)
 
     return str(resp)
 
@@ -59,8 +59,8 @@ def voice():
 def address(number):
     resp = VoiceResponse()
     resp.say("How many people are stranded there?", voice='alice')
-    resp.record(action=URL+'thanks',
-            recordingStatusCallback=URL+"completed/capacity/"+number)
+    resp.record(action=URL + 'thanks',
+                recordingStatusCallback=URL + "completed/capacity/" + number)
 
     return str(resp)
 
@@ -96,19 +96,19 @@ def sms_survey():
     response = MessagingResponse()
     from_number = request.values.get('From')
 
-    if (from_number not in log.keys()):
+    if from_number not in log.keys():
         response.message("Please enter your address.")
         log[from_number] = {'address': None, 'capacity': None}
 
-    elif (log[from_number]['address'] is None):
+    elif log[from_number]['address'] is None:
         log[from_number]['address'] = request.values.get('Body')
         response.message("How many people are stranded there?")
 
-    elif (log[from_number]['capacity'] is None):
+    elif log[from_number]['capacity'] is None:
         cap = request.values.get('Body')
         try:
             cap = int(cap)
-            if (cap <= 0):
+            if cap <= 0:
                 response.message("Please enter a valid number.\n\nHow many people are stranded there?")
             else:
                 log[from_number]['capacity'] = cap
@@ -141,7 +141,8 @@ def web():
 
 @app.route("/test", methods=['POST', 'GET'])
 def test():
-    url = 'https://api.twilio.com/2010-04-01/Accounts/ACcfef691231a0b455b289bdd859eefd71/Recordings/RE0f8ab886b7428c8419e10fb000cd4b44' + '.wav'
+    url = 'https://api.twilio.com/2010-04-01/Accounts/ACcfef691231a0b455b289bdd859eefd71/Recordings' \
+          '/RE0f8ab886b7428c8419e10fb000cd4b44' + '.wav '
     number = '1234'
     log[number] = {'address': None, 'capacity': None}
     process(url, number, 'address')
@@ -163,7 +164,8 @@ def process(url, number, key):
     if key == 'address':
         transcript = openai.Completion.create(
             engine='text-davinci-001',
-            prompt='This is an incorrect transcript of a UK address: \"' + transcript + '\"\nThe correct address with a corrected street name and matching post code is:',
+            prompt='This is an incorrect transcript of a UK address: \"' + transcript
+                   + '\"\nThe correct address with a corrected street name and matching post code is:',
             temperature=0.0,
             max_tokens=64,
             top_p=1,
